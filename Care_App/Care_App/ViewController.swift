00d5db5c91b3b1ae09662ec2df07802c35716cda
devNasm <naseem.oyebola@fcmb.com>
2024-06-17 10:09:10 +0100

    feat: Project initialization

---
+++ Care_App/Care_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Care_App
+//
+//  Created by Naseem Oyebola on 17/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
